a9369335767c80efed626f15d1897aab69373ef0
Matthew Orzechowski <morzechowski@berkeley.edu>
2016-03-18 20:48:12 -0700

    Update pokemons_controller.rb

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -10,34 +10,34 @@ class PokemonsController < ApplicationController
 	def damage
 		@pokemon = Pokemon.find(params[:id]) 
 		@pokemon.health -= 10
-    if @pokemon.health <= 0
-      @pokemon.delete
-    else
-		  @pokemon.save
-    end
-		  redirect_to trainer_path(current_trainer.id)
-  end
+		if @pokemon.health <= 0
+      			@pokemon.delete
+    		else
+		 	@pokemon.save
+		end
+		redirect_to trainer_path(current_trainer.id)
+	 end
 
  	def new
-    @pokemon = Pokemon.new
-  end
+    		@pokemon = Pokemon.new
+	 end
 
 
-  def create
-    @pokemon = Pokemon.new(pokemon_params)
-    @pokemon.health = 100
-  	@pokemon.level = 1
-  	@pokemon.trainer_id = current_trainer.id
-    if @pokemon.save
-      redirect_to current_trainer
-    else
-      redirect_to pokemon_new_path, :error => flash[:error] = @pokemon.errors.full_messages.to_sentence
-    end
-  end
-
-  private
-  def pokemon_params
-    params.require(:pokemon).permit(:name)
-  end
+  	def create
+	    	@pokemon = Pokemon.new(pokemon_params)
+	    	@pokemon.health = 100
+	  	@pokemon.level = 1
+	  	@pokemon.trainer_id = current_trainer.id
+	    if @pokemon.save
+	      redirect_to current_trainer
+	    else
+	      redirect_to pokemon_new_path, :error => flash[:error] = @pokemon.errors.full_messages.to_sentence
+	    end
+	  end
+	
+	  private
+	  def pokemon_params
+	    params.require(:pokemon).permit(:name)
+	  end
 
 end
